d94e8e5d8c7af7196310d955f3fd0ac0a6084b1b
Steve Meskens <steve@mezkenz.com>
2025-08-23 23:52:24 +0200

    fix: disable typedRoutes to allow dynamic locale links

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true, serverActions: { allowedOrigins: ['*'] } },
+  // experimental: { typedRoutes: true, serverActions: { allowedOrigins: ['*'] } },
+  experimental: { serverActions: { allowedOrigins: ['*'] } },
 };
 export default nextConfig;
