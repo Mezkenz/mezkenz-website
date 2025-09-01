269c5e144087db922765e25d95c37703900203a3
Steve Meskens <steve@mezkenz.com>
2025-09-01 23:16:22 +0200

    support button without radix slot

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
@@ -17,12 +16,19 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> { asChild?: boolean }
+  VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
-    return <Comp className={cn(buttonVariants({ variant }), className)} ref={ref} {...props} />;
+  ({ className, variant, asChild, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant }), className)}
+        ref={ref}
+        {...props}
+      />
+    );
   }
 );
 Button.displayName = "Button";
